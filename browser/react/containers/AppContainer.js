9308d2a39755c3597538c57842cdcecdac19fd4a
xpolb1 <bogdan.polovko@gmail.com>
2016-12-02 10:53:31 -0500

    Created props.children

--- browser/react/containers/AppContainer.js
+++ browser/react/containers/AppContainer.js
@@ -109,19 +109,17 @@ export default class AppContainer extends Component {
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
         <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <Album
-            album={this.state.selectedAlbum}
-            currentSong={this.state.currentSong}
-            isPlaying={this.state.isPlaying}
-            toggleOne={this.toggleOne}
-          /> :
-          <Albums
-            albums={this.state.albums}
-            selectAlbum={this.selectAlbum}
-          />
-        }
+          {
+            this.props.children ?
+            React.cloneElement(this.props.children, {
+                album: this.state.selectedAlbum,
+                currentSong: this.state.currentSong,
+                sPlaying: this.state.isPlaying,
+                toggleOne: this.toggleOne,
+                albums: this.state.albums,
+                selectAlbum: this.selectAlbum
+            }) : null
+          }
         </div>
         <Player
           currentSong={this.state.currentSong}
@@ -136,3 +134,16 @@ export default class AppContainer extends Component {
     );
   }
 }
+
+
+// this.state.selectedAlbum.id ?
+//           <Album
+//             album={this.state.selectedAlbum}
+//             currentSong={this.state.currentSong}
+//             isPlaying={this.state.isPlaying}
+//             toggleOne={this.toggleOne}
+//           /> :
+//           <Albums
+//             albums={this.state.albums}
+//             selectAlbum={this.selectAlbum}
+//           />
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import { Router, Route, hashHistory } from 'react-router';
 
 ReactDOM.render(
-  <AppContainer />,
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer}>
+
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
